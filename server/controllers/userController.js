924d3952815e6a74611c1ec5715f358244317655
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-01 16:48:22 +0530

    Added authenticate user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,41 @@
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+
+  const user = await User.findOne({ email });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (user && isMatch) {
+    const userId = user._id;
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: false,
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+});
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
-import express  from "express";
-import {registerUser} from "../controllers/userController.js";
+import express from "express";
+import { authUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post('/', registerUser);
+router.post("/", registerUser);
+router.post("/auth", authUser);
 
 export default router;
