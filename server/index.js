f5af9042a9cb245c9252a0b9ff8fe33e117da1dc
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-01 16:43:29 +0530

    Created register user endpoint

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Routes
+app.use("/api/users",userRoutes); 
 
 // Database Connection
 connectDB();
