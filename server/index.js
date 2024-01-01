0573d345ce5879769c376b695c04066ad17e3b5e
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-01 17:33:41 +0530

    Added error middlewares

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 const app = express();
@@ -13,7 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
-app.use("/api/users",userRoutes); 
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 // Database Connection
 connectDB();
