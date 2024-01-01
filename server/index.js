542185cefe6b8d003c5e15ea849c0e7b2d8fb185
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-01 14:36:40 +0530

    Added required middlewares

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Database Connection
 connectDB();
 
-app.get('/',(req,res)=> res.send('Server is ready'));
+app.get("/", (req, res) => res.send("Server is ready"));
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
