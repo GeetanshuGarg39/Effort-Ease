265c176d5ea9de3147252e3bbaaac7851b3efc1d
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-02 12:07:51 +0530

    Designed header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const header = () => {
+  return (
+    <div className="h-14 flex items-center justify-between px-6 sm:px-16 font-raleway border-y">
+      <div className=" font-bold">Elite Ease</div>
+
+      <div className="flex gap-3 font-bold items-center">
+        <Link to="/" className="flex bg-black text-white rounded-lg p-2 " >Login</Link>
+        <Link to="/" className=" bg-blue-500 text-white rounded-lg p-2">Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default header;
