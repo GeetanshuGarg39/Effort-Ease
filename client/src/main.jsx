596821e54df2f8e719e21f4bb67c893f61de6de8
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-02 12:50:18 +0530

    Designed register screen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,10 +9,12 @@ import {
 import App from "./App.jsx";
 import "./index.css";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import RegisterScreen from "./screens/RegisterScreen.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />}>
     <Route  index={true} path="/" element = {<HomeScreen />} />
+    <Route  index={true} path="/register" element = {<RegisterScreen />} />
   </Route>)
 );
 
