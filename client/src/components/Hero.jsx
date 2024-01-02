3a0045bdf85a968f50de2e79c39526be17774125
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-01-02 12:20:13 +0530

    Designed hero component

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LoginCard from './LoginCard'
+import homeImg from "../assets/home.avif"
+
+const Hero = () => {
+  return (
+    <>
+    <div className='flex justify-around items-center font-raleway p-7 md:p-4 md:pl-6'>
+        <div className='flex flex-col gap-4 lg:gap-20 '>
+            <h1 className=' text-2xl font-semibold md:text-3xl lg:text-4xl'>Effortlessly Manage Your Tasks</h1>
+            <LoginCard />
+        </div>
+      <img className=' hidden md:block md:w-1/2 lg:w-auto' src={homeImg}></img>
+    </div>
+    </>
+  )
+}
+
+export default Hero
